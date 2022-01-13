22acb630572cd70033e7e3ee61c9389faa2c9215
Lambert <lambertgithub@gmail.com>
2022-01-13 02:26:38 +0200

    New

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,14 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         $this->call(VehicleTypeSeeder::class);
         $this->call(VehicleCategorySeeder::class);
         $this->call(DriverSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(DepartmentSeeder::class);
         $this->call(VehicleSeeder::class);
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
